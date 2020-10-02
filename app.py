03d7ef2cea3dac7c088eefc7792c1d8d0cd3d20f
Moisés Oliveira <moises.oliveira@ecomp.ufsm.br>
2020-10-01 23:53:38 -0300

    just ajusts

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ wordfy = Flask(__name__)
 @wordfy.route('/<n>')
 def num2txt(n):
     try:
-        return {'extenso': num2words(n,lang='pt_BR').replace(',',' e')}, 200
+        # Foi utilizada a biblioteca num2words disponivel em
+        # https://pypi.org/project/num2words/
+        words = {'extenso': num2words(n,lang='pt_BR').replace(',',' e')}
+        return words, 200
     except:
         return '', 404
+
 
 if __name__ == '__main__':
     wordfy.run(debug=True, host='0.0.0.0',port=3000)
